81f1e2ea20e7faed73d764621952a922d91ab01f
Sukhaman Singh <sukhamandev@gmail.com>
2024-02-11 10:04:33 -0500

    LoginVC added

---
+++ Decisive/Login/Views/LoginVC.swift
@@ -0,0 +1,15 @@
+//
+//  LoginVC.swift
+//  Decisive
+//
+//  Created by Sukhaman Singh on 2/11/24.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    override func viewDidLoad() {
+        view.backgroundColor = .systemBackground
+    }
+}
