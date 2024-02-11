25a15c21c12ae393f0857a830fa06cad65b5b94f
Sukhaman Singh <sukhamandev@gmail.com>
2024-02-11 14:17:12 -0500

    LoginView UI updated with snapshot tests and Colors asset file added

--- Decisive/Login/Views/LoginVC.swift
+++ Decisive/Login/Views/LoginVC.swift
@@ -6,13 +6,24 @@ import UIKit
 
 class LoginVC: UIViewController {
     var backgroundAvatar = UIImageView(frame: .zero)
+    var loginView = UIView()
+    var appLogoAvatar = UIImageView(frame: .zero)
+    var txtEmail = CustomTextField(placeholder: "Email")
+    var txtPassword = CustomTextField(placeholder: "Password")
+    var btnLogin = CustomButton(backgroundColor: .appOrangePrimary, title: "Sign In")
+    var btnSignup = CustomButton(backgroundColor: .clear, title: "Create Account",titleColor: .appDarkBluePrimary)
     
     override func viewDidLoad() {
-        view.backgroundColor = .systemBackground
         self.addBackgroundImageView()
+        self.addLoginView()
+        addAppLogoAvatarView()
+        addEmailTextField()
+        addPasswordTextField()
+        addSignInButton()
+        addSignUpButton()
     }
     
-    func addBackgroundImageView() {
+   fileprivate func addBackgroundImageView() {
         self.view.addSubview(self.backgroundAvatar)
         self.backgroundAvatar.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
@@ -22,5 +33,87 @@ class LoginVC: UIViewController {
             self.backgroundAvatar.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
         ])
         self.backgroundAvatar.image = UIImage(resource: .samaraDoole)
+    }
+    
+   fileprivate func addLoginView() {
+        self.view.addSubview(self.loginView)
+        self.loginView.backgroundColor = .systemBackground
+        self.loginView.translatesAutoresizingMaskIntoConstraints = false
+        let width = self.view.frame.size.width * 0.7
+        NSLayoutConstraint.activate([
+            self.loginView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            self.loginView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            self.loginView.widthAnchor.constraint(equalToConstant: width),
+            self.loginView.heightAnchor.constraint(equalToConstant: 320)
+        ])
+       
+       self.loginView.layer.shadowPath = UIBezierPath(rect: self.loginView.bounds).cgPath
+       self.loginView.layer.shadowColor = UIColor.lightGray.cgColor
+       self.loginView.layer.shadowOpacity = 2
+       self.loginView.layer.shadowOffset = .zero
+       self.loginView.layer.shadowRadius = 5
+       self.loginView.layoutSubviews()
+       self.loginView.layer.cornerRadius = 10
+    }
+    
+   fileprivate func addAppLogoAvatarView() {
+        self.view.addSubview(self.appLogoAvatar)
+        self.appLogoAvatar.translatesAutoresizingMaskIntoConstraints = false
+        self.appLogoAvatar.image =  UIImage(resource: .appIcon)
+        NSLayoutConstraint.activate([
+            self.appLogoAvatar.topAnchor.constraint(equalTo: self.loginView.topAnchor, constant: 20),
+            self.appLogoAvatar.centerXAnchor.constraint(equalTo: self.loginView.centerXAnchor),
+            self.appLogoAvatar.widthAnchor.constraint(equalToConstant: 60),
+            self.appLogoAvatar.heightAnchor.constraint(equalToConstant: 60)
+        ])
+       self.appLogoAvatar.layer.shadowPath = UIBezierPath(rect: self.appLogoAvatar.bounds).cgPath
+       self.appLogoAvatar.layer.shadowColor = UIColor.lightGray.cgColor
+       self.appLogoAvatar.layer.shadowOpacity = 2
+       self.appLogoAvatar.layer.shadowOffset = .zero
+       self.appLogoAvatar.layer.shadowRadius = 5
+       self.appLogoAvatar.layoutSubviews()
+       self.appLogoAvatar.layer.cornerRadius = 10
+    }
+    
+   fileprivate func addEmailTextField() {
+        self.loginView.addSubview(txtEmail)
+        
+        NSLayoutConstraint.activate([
+            self.txtEmail.topAnchor.constraint(equalTo: self.appLogoAvatar.bottomAnchor, constant: 20),
+            self.txtEmail.leadingAnchor.constraint(equalTo: self.loginView.leadingAnchor, constant: 20),
+            self.txtEmail.trailingAnchor.constraint(equalTo: self.loginView.trailingAnchor,constant: -20),
+            self.txtEmail.heightAnchor.constraint(equalToConstant: 35)
+        ])
+    }
+    
+   fileprivate func addPasswordTextField() {
+        self.loginView.addSubview(txtPassword)
+        NSLayoutConstraint.activate([
+            self.txtPassword.topAnchor.constraint(equalTo: self.txtEmail.bottomAnchor, constant: 20),
+            self.txtPassword.leadingAnchor.constraint(equalTo: self.loginView.leadingAnchor, constant: 20),
+            self.txtPassword.trailingAnchor.constraint(equalTo: self.loginView.trailingAnchor,constant: -20),
+            self.txtPassword.heightAnchor.constraint(equalToConstant: 35)
+        ])
+    }
+    
+   fileprivate func addSignInButton() {
+        self.loginView.addSubview(btnLogin)
+        NSLayoutConstraint.activate([
+            self.btnLogin.topAnchor.constraint(equalTo: self.txtPassword.bottomAnchor, constant: 20),
+            self.btnLogin.centerXAnchor.constraint(equalTo: self.txtPassword.centerXAnchor),
+            self.btnLogin.widthAnchor.constraint(equalToConstant: 120),
+            self.btnLogin.heightAnchor.constraint(equalToConstant: 45)
+        ])
+        btnLogin.layer.cornerRadius = 22.5
+    }
+    
+   fileprivate func addSignUpButton() {
+        self.loginView.addSubview(btnSignup)
+        NSLayoutConstraint.activate([
+            self.btnSignup.topAnchor.constraint(equalTo: self.btnLogin.bottomAnchor, constant: 10),
+            self.btnSignup.leadingAnchor.constraint(equalTo: self.loginView.leadingAnchor, constant: 20),
+            self.btnSignup.trailingAnchor.constraint(equalTo: self.loginView.trailingAnchor,constant: -20),
+            self.btnSignup.heightAnchor.constraint(equalToConstant: 45)
+        ])
     }
 }
--- DecisiveTests/Login/SnapshotTests/LoginSnapshotTests.swift
+++ DecisiveTests/Login/SnapshotTests/LoginSnapshotTests.swift
@@ -9,14 +9,14 @@ class LoginSnapshotTests: XCTestCase {
     
     func test_loginVC_iPhoneDevice() {
         let sut = makeSUT()
-        record(snapshot: sut.snapshot(for: .iPhone(style: .dark)), named: "LOGIN_DARK_VIEW_iPhone")
-        record(snapshot: sut.snapshot(for: .iPhone(style: .light)), named: "LOGIN_LIGHT_VIEW_iPhone")
+        assert(snapshot: sut.snapshot(for: .iPhone(style: .dark)), named: "LOGIN_DARK_VIEW_iPhone")
+        assert(snapshot: sut.snapshot(for: .iPhone(style: .light)), named: "LOGIN_LIGHT_VIEW_iPhone")
     }
     
     func test_loginVC_iPadDevice() {
         let sut = makeSUT()
-        record(snapshot: sut.snapshot(for: .iPad(style: .dark)), named: "LOGIN_DARK_VIEW_iPad")
-        record(snapshot: sut.snapshot(for: .iPad(style: .light)), named: "LOGIN_LIGHT_VIEW_iPad")
+        assert(snapshot: sut.snapshot(for: .iPad(style: .dark)), named: "LOGIN_DARK_VIEW_iPad")
+        assert(snapshot: sut.snapshot(for: .iPad(style: .light)), named: "LOGIN_LIGHT_VIEW_iPad")
     }
     
     // MARK: Helpers
