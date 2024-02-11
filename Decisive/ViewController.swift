07f3f15df4e94e57789e1c9928c1e88466b93b81
Sukhaman Singh <sukhamandev@gmail.com>
2024-02-11 09:37:27 -0500

    Initial Commit

---
+++ Decisive/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Decisive
+//
+//  Created by Sukhaman Singh on 2/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
