39ce0418e946877fb082f5ab738895916c51c6f6
Sukhaman Singh <sukhamandev@gmail.com>
2024-02-13 22:10:36 -0500

    Added LoginUIComposer

--- Decisive/Login/Composer/LoginUIComposer.swift
+++ Decisive/Login/Composer/LoginUIComposer.swift
@@ -1,8 +1,14 @@
 //
-//  LoginUIComposer.swift
-//  Decisive
-//
 //  Created by Sukhaman Singh on 2/13/24.
 //
 
 import Foundation
+
+struct LoginUIComposer {
+    
+    static func createLoginModule(_ viewModel: LoginViewModel) -> LoginVC {
+        let view = LoginVC()
+        view.viewModel = viewModel
+        return view
+    }
+}
